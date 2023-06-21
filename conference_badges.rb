7dd1147097dc15fff92e54f8d6691cc5757e0d63
Philip Demand <philipdemand@yahoo.com>
2023-06-21 14:55:16 -0400

    commit tree 497d27444e15f6f06e3130cdc67c08a387760466

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    messages = []
+  
+  array.each do |name|
+    messages << "Hello, my name is #{name}."
+  end
+  
+  messages
+end
+
+def assign_rooms(array)
+  assigned_message = []
+  array.each_with_index do |name, index|
+  assigned_message.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
+end
+  assigned_message
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |message|
+    puts message
+  end
+
+  assign_rooms(array).each do |message|
+    puts message
+  end
+end
